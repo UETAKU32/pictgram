38237d83b25859e9806868f7cedd3ecda3e6027e
uetaku <takuya.uehara.320@gmail.com>
2023-06-12 00:23:16 +0000

    Rails 04課題、userにバリテーションを追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,9 @@
 class User < ApplicationRecord
-  validates :name, presence: true
-  validates :email, presence: true
+  validates :name, presence: true, length: { maximum: 15 }
+  validates :email, presence: true,format: { with: /\A[^@\s]+@[^@\s]+\.[^@\s]+\z/, message: "有効なアドレスを記入してください" }
   
   has_secure_password
+  validates :password, length: { in: 8..32 ,format: { with: /\A(?=.*[a-zA-Z])(?=.*\d)/, message: "アルファベットと数字を1文字ずつ入れてください" }}
+  
+  
 end
