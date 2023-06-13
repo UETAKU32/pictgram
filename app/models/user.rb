7d6ff7266e1acaf587c299cc4d3d6f98c7ed6b92
uetaku <takuya.uehara.320@gmail.com>
2023-06-13 10:27:55 +0000

    Rails 06 写真投稿機能の作成_1作業内容追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,5 +5,5 @@ class User < ApplicationRecord
   has_secure_password
   validates :password, length: { in: 8..32} ,format: { with: /\A(?=.*[a-zA-Z])(?=.*\d)/, message: "アルファベットと数字を1文字ずつ入れてください" }
   
-  
+  has_many :topics
 end
